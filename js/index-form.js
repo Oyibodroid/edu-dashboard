8dfccf67f58f453ce056bbdd40083742e6ff8d3c
OyiboDroid <oyibodaniel247@gmail.com>
2024-06-04 19:37:09 +0100

    commission

--- js/index-form.js
+++ js/index-form.js
@@ -1,22 +1,37 @@
 const logOption = document.querySelectorAll(".option");
-const hamBurger = document.querySelector('#hamburger');
-const sideNav = document.querySelector('#side-nav');
+const hamBurger = document.querySelector("#hamburger");
+const sideNav = document.querySelector("#side-nav");
+const leftSide = document.querySelector("#main");
+const burger = document.querySelectorAll(".burger");
 
 for (let i = 0; i < logOption.length; i++) {
   const element = logOption[i];
   element.onclick = function () {
-    if(element.classList('active')){
-      element.classList.remove('active')
+    if (element.classList("active")) {
+      element.classList.remove("active");
     }
     element.classList.add("active");
     console.log(i + 1);
   };
 }
 
+hamBurger.addEventListener("click", function () {
+  sideNav.classList.toggle("hide");
+  leftSide.classList.toggle("hide");
+  console.log("helo");
 
-hamBurger.addEventListener('click', function(){
-  sideNav.classList.toggle('hide')
-  console.log('helo');
-})
+  if (sideNav.classList != "hide" && leftSide.classList != "hide") {
+    console.log("hello");
+    for (let b = 0; b < burger.length; b++) {
+      const element = burger[b];
+      element.classList.add("hide");
+    }
+  }else{
+    for (let b = 0; b < burger.length; b++) {
+      const element = burger[b];
+      element.classList.remove('hide')
+    }
+    }
+});
 
 console.log(logOption);
